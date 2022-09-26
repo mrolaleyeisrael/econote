9686c6f71b1344daba1712c74e93bcf8f73f1f1e
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-09-26 02:28:41 +0100

    second attempt

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
+let boldBtn= document.getElementById('bold');
 let copyBtn = document.getElementById('copy');
+let underlineBtn = document.getElementById("underline");
 let text=document.getElementById('textarea');
+let addBtn = document.getElementById("add");
+let title = document.getElementById("title");
+
+//! Variables and Array
+let notes = [];
+let notesToAdd = {};
+let textAreaInput = "";
+let titleInput = "";
+
+//! Input Handler
+text.addEventListener("change", (e) => {
+	textAreaInput = e.target.value.trim();
+});
+
+title.addEventListener("change", (e) => {
+	titleInput = e.target.value.trim();
+});
+
+
+//! Save Button handler
+function saveNote() {
+	if (textAreaInput.length > 0 && titleInput.length > 0) {
+		notesToAdd = {
+			title: titleInput,
+			summary: textAreaInput,
+		};
+		notes.push(notesToAdd);
+		text.value = "";
+		title.value = "";
+		display();
+	} else if (titleInput.length === 0) {
+		alert("❌ Title is Empty ❌");
+	} else {
+		alert("❌ Textarea is Empty ❌");
+	}
+}
+addBtn.addEventListener("click", saveNote);
+
+console.log(notesToAdd)
+
+
+
+// let textArea = document.getElementById("textarea");
 
 copyBtn.onclick = function(){
 	textarea.select();
@@ -7,9 +52,71 @@ copyBtn.onclick = function(){
     
 }
 
-let boldBtn= document.getElementById('bold');
  boldBtn.onclick = function() {
  	
  	textarea.style.fontWeight ="900";
  	
  }
+
+
+//! Notes Added Displayed
+function truncate(str, n) {
+	return str?.length > n ? str.substr(0, n - 1) + " ..." : str;
+}
+
+function display() {
+	let notesToBeDisplay = "";
+	notes.forEach((element, index) => {
+		notesToBeDisplay += `
+    <div class="card" id=${index} onclick="switchCase(this.id)" >
+    <span>${element.title}</span>
+    <p>${truncate(element.summary, 50)}</p>
+    </div>`;
+	});
+
+	if (notes.length !== 0) {
+		document.getElementById("display-notes").innerHTML = notesToBeDisplay;
+	}
+}
+
+
+//! UnderLine
+function underLineFunc() {
+	let selectedText = window.getSelection().toString();
+	// console.log(selectedText);
+	if (selectedText === "") {
+		alert("❌ Text Not Selected ❌");
+	} else if (selectedText.includes("<u>")) {
+		let newText = selectedText.replace("<u>", "");
+		newText = newText.replace("</u>", "");
+		textAreaInput = textAreaInput.replace(selectedText, newText);
+		// textAreaInput = newText;
+	} else {
+		let newText = `<u>${selectedText}</u>`;
+		textAreaInput = textAreaInput.replace(selectedText, newText);
+		// textAreaInput = newText;
+	}
+}
+
+underlineBtn.addEventListener("click", underLineFunc);
+
+//! Checking KeyPress of Ctrl + B & Ctrl + U
+let keysPressed = {};
+
+text.addEventListener("keydown", (event) => {
+	keysPressed[event.keyCode] = true;
+
+	if (keysPressed["17"] && event.keyCode === 66) {
+		// alert(event.key);
+		boldFunc();
+		// console.log("Ctrl + B");
+	}
+	if (keysPressed["17"] && event.keyCode === 85) {
+		// alert(event.key);
+		underLineFunc();
+		// console.log("Ctrl + B");
+	}
+});
+
+
+
